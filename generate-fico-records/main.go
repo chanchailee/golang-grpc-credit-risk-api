bddfe69ef9360ad31bb774043c679d3eb06a2df5
chanchailee <chanchai.lee@live.com>
2019-12-03 15:08:58 +0700

    add read argument function

--- generate-fico-records/main.go
+++ generate-fico-records/main.go
@@ -14,18 +14,46 @@ command:
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/chanchailee/golang-grpc-credit-risk-api/pkg/mockrecords"
 )
 
 var (
-	filename = "../mock-fico-records.csv"
-	size     = 50
+	size           = 50
+	errMissingArgs = fmt.Errorf("error : missing argument")
+	errInvalidArgs = fmt.Errorf("error : invalid argument")
 )
 
+func readArgs(args []string) (string, string, error) {
+	length := len(args)
+
+	switch {
+	case length < 3:
+		return "", "", errMissingArgs
+	case length == 3:
+		return strings.TrimSpace(args[1]), strings.TrimSpace(args[2]), nil
+	default:
+		return "", "", errInvalidArgs
+	}
+}
+
 func main() {
-	err := mockrecords.Generate(size, filename)
+	filename, s, err := readArgs(os.Args)
+	if err != nil {
+		log.Fatalf("read argument error: %s", err)
+	}
+
+	size, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("convert string to int error: %s", err)
+	}
+
+	err = mockrecords.Generate(size, filename)
 	if err != nil {
 		log.Fatalf("error: can't generate csv file %+v", err)
 	}
