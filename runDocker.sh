3a4963fc9ea6da435e8a35f3a19cc11b5fb7548f
chanchailee <chanchai.lee@live.com>
2019-12-03 15:14:10 +0700

    add shellscript to run docker

---
+++ runDocker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build . -f app.Dockerfile -t generate-fico-records
+docker run -v $PWD:/go/src/github.com/chanchailee/golang-grpc-credit-risk-api/generate-fico-records/ -it generate-fico-records generate-fico-records $1 $2
